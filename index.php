516af4989ea92d961cfc5a6ba72a582f1d47ea0d
Willian Moro Conk <willmgrfu@gmail.com>
2018-04-14 23:49:23 -0300

    Devmind commit in 2018-04-14 23:49:23

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<!-- Styling for public area -->
 	<link rel="stylesheet" href="static/css/public_styling.css">
 	<meta charset="UTF-8">
-	<title>Willian Conk| Home </title>
+	<title>Programing| Home </title>
 </head>
 <body>
 	<!-- container - wraps whole page -->
@@ -14,7 +14,7 @@
 		<!-- navbar -->
 		<div class="navbar">
 			<div class="logo_div">
-				<a href="index.php"><h1>Programing</h1></a>
+				<a href="index.php"><h1>Willian Conk</h1></a>
 			</div>
 			<ul>
 			  <li><a class="active" href="index.php">Home</a></li>
